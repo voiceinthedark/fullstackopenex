5166c60eb59469695030980384ed58fa57cc6ffb
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-10-22 19:30:36 +0300

    Exercise 1.6 added

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,8 +1,39 @@
-import './App.css';
+import React, { useState } from "react";
+
+const Heading = (props) => {
+  return (
+    <h1>{props.text}</h1>
+  )
+};
+
+const Button = (props) => {
+  return(
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+};
+
+const Stat = (props) => {
+  return(
+    <p>{props.name} {props.val}</p>
+  )
+}
 
 function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
-    <div className="App">      
+    <div>
+        <Heading text="Give Feedback" />
+        <Button text="Good" handleClick={() => setGood(good + 1)}/>
+        <Button text="Neutral" handleClick={() => setNeutral(neutral + 1)}/>
+        <Button text="Bad" handleClick={() => setBad(bad + 1)}/>
+        <Heading text="Statistics" />
+        <Stat name="good" val={good} />
+        <Stat name="neutral" val={neutral} />
+        <Stat name="bad" val={bad} />
+    
     </div>
   );
 }
