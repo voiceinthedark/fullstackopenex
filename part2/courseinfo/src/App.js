f296c539f87464ebe82021f16b05dd3c81600ce2
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-10-23 19:37:46 +0300

    ex 2.5 seperate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,57 +1,5 @@
 import './App.css';
-
-const Total = ({course}) => {
-  console.log(course.parts)
-  return (
-    <>
-      <li><strong>
-        Total of exercises{" "}
-        {course.parts.map((a) => a.exercises).reduce((a, b) => a + b, 0)}
-      </strong>
-      </li>
-    </>
-  );
-};
-
-const Content = ({course}) => {
-  return (
-    <>
-      <ul>
-      {course.parts.map((p) => <Part key={p.id} part={p.name} exercises={p.exercises}/>)}        
-      <Total course={course} />
-      </ul>
-    </>
-  );
-};
-
-const Header = ({name}) => {
-  return <h1>{name}</h1>;
-};
-
-const Part = (props) => {
-  return (
-    <> 
-    <li key={props.id}>     
-        {props.part} {props.exercises}      
-      </li>
-    </>
-  );
-};
-
-const Courses = ({courses}) =>{
-  return(
-    <>
-    {courses.map(course => {
-      return(
-        <div key={course.id}>
-        <Header name={course.name} />
-        <Content course={course} />
-        </div>
-    )})
-    }
-    </>
-  );
-};
+import Courses from './components/courses';
 
 const App = () => {
   const courses = [
