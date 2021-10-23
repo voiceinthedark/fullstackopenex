3fda124598c6ee85371c7ebc338d5c6aee353fa1
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-10-23 19:08:27 +0300

    ex 2.3

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -4,10 +4,11 @@ const Total = ({course}) => {
   console.log(course.parts)
   return (
     <>
-      <strong>
+      <li><strong>
         Total of exercises{" "}
         {course.parts.map((a) => a.exercises).reduce((a, b) => a + b, 0)}
       </strong>
+      </li>
     </>
   );
 };
@@ -17,6 +18,7 @@ const Content = ({course}) => {
     <>
       <ul>
       {course.parts.map((p) => <Part key={p.id} part={p.name} exercises={p.exercises}/>)}        
+      <Total course={course} />
       </ul>
     </>
   );
@@ -40,8 +42,7 @@ const Course = ({course}) =>{
   return(
     <>
     <Header name={course.name} />
-    <Content course={course} />
-    <Total course={course} />
+    <Content course={course} />    
     </>
   );
 };
