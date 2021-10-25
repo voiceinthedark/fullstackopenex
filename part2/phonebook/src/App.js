399a87d419ba330724ddd327ad682d891bc693a2
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-10-25 16:14:23 +0300

    Ex 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -7,14 +7,18 @@ const Person = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "33-333333" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1, show: true },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2, show: true },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3, show: true },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4, show: true },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterBy, setFilterBy] = useState("");
 
   const addName = (event) => {
-    event.preventDefault();
-    // console.log(`Adding name`, newName);
-    // console.log(persons.map(p => p.name).find((a) => a === 'Art Hellas'));
+    event.preventDefault();    
     if (persons.map((p) => p.name).find((a) => a === newName)) {
       alert(`${newName} is already in the phonebook`);
       return;
@@ -23,6 +27,8 @@ const App = () => {
     const newPerson = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1,
+      show: true,
     };
 
     setPersons(persons.concat(newPerson));
@@ -31,7 +37,6 @@ const App = () => {
   };
 
   const handleNameChange = (event) => {
-    // console.log(event.target.value);
     setNewName(event.target.value);
   };
 
@@ -39,15 +44,33 @@ const App = () => {
     setNewNumber(e.target.value);
   }
 
+  const handleFilterChange = (e) => {
+    setFilterBy(e.target.value);
+    persons    
+    .forEach((p) => p.name.toLowerCase()
+      .indexOf(e.target.value.toLowerCase()) > -1 ? p.show = true : p.show = false);
+    // console.log(persons);
+    setPersons(persons);
+  }
+
+  // const showPersons = filterBy === "" ?
+  //   persons :
+  //   persons.filter(p => p.show === true);
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        Filter by name:
+        <input value={filterBy} onChange={handleFilterChange} />
+      </div>
+      <h2>Add a new entry</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-        number: <input value={newNumber} onChange={handleNumberChange}/>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -55,7 +78,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(p => <Person key={p.name} name={p.name} number={p.number} />)}      
+        {persons
+          .filter((p) => p.show === true)
+          .map((p) => (
+            <Person key={p.id} name={p.name} number={p.number} />
+          ))}
       </ul>
     </div>
   );
