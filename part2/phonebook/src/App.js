8034592cdc2acc69252e28c5be1d1b800eee4c47
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-10-26 19:04:22 +0300

    Ex. 2.11, phonebook from server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import Form from "./components/Form";
 import { Persons } from "./components/Persons";
+import axios from 'axios';
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1, show: true },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2, show: true },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3, show: true },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4, show: true },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterBy, setFilterBy] = useState("");
@@ -47,14 +43,19 @@ const App = () => {
     let filterArr = [...persons];
     filterArr    
     .forEach((p) => p.name.toLowerCase()
-      .indexOf(e.target.value.toLowerCase()) > -1 ? p.show = true : p.show = false);
-    // console.log(persons);
+      .indexOf(e.target.value.toLowerCase()) > -1 ? p.show = true : p.show = false);    
     setPersons(filterArr);
-  }
+  }  
 
-  // const showPersons = filterBy === "" ?
-  //   persons :
-  //   persons.filter(p => p.show === true);
+  useEffect(() => {
+    console.log("Effect...");
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('response :>> ', response.status);
+      console.log(response.data);
+      setPersons(response.data);
+    })
+  }, []);
 
   return (
     <div>
