5a73a2ba0d188a65f82f10bc5507567b0e89609f
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-10-30 12:44:53 +0300

    notes backend setup

--- part3/notes_node/index.js
+++ part3/notes_node/index.js
@@ -1,4 +1,8 @@
-const http = require("http");
+const http = require('http');
+const express = require("express");
+const app = express();
+
+app.use(express.json());
 
 let notes = [
   {
@@ -20,11 +24,64 @@ let notes = [
     important: true,
   },
 ];
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World</h1>");
 });
 
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = +request.params.id;
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {    
+    response
+      .status(404)
+      .send("<h3>Record does not exist</h3>")
+      .end();
+  }
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = +request.params.id;
+  notes = notes.filter(note => note.id !== id);
+
+  response.status(204).end();
+})
+
+app.post('/api/notes', (request, response) => {  
+
+  const body = request.body;
+  
+  if(!body.content){
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+    
+  notes = notes.concat(note);
+  response.json(note);
+})
+
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+function generateId() {
+  return notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0;
+}
+
