04b5f3fa6d367019d6922250cbd179fa96e496ce
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-10-30 23:51:32 +0200

    Ex 3.8 Add custom morgan logging

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,9 +1,44 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan")
+const morgan = require("morgan");
 
 app.use(express.json());
-app.use(morgan('tiny'))
+
+/**
+ * Capturing error regarding headers
+ * ERR_HTTP_HEADERS_SENT
+ */
+app.use((req, res, next) => {
+  const render = res.render;
+  const send = res.send;
+  res.render = function renderWrapper(...args) {
+    Error.captureStackTrace(this);
+    return render.apply(this, args);
+  };
+  res.send = function sendWrapper(...args) {
+    try {
+      send.apply(this, args);
+    } catch (err) {
+      console.error(
+        `Error in res.send | ${err.code} | ${err.message} | ${res.stack}`
+      );
+    }
+  };
+  next();
+});
+
+//Setup a morgan token to return request body
+morgan.token("req_body", (req) => {
+  return req.content;
+});
+
+app.use(assignContent);
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :req_body"
+  )
+);
+
 
 let persons = [
   {
@@ -28,74 +63,87 @@ let persons = [
   },
 ];
 
-app.get('/', (request, response) => {
-    response.send("<h2>Hello World</h2>")    
-})
+app.get("/", (request, response) => {
+  response.send("<h2>Hello World</h2>");
+});
 
-app.get('/api/persons', (request, response) => {
-    response.status(200).json(persons);    
-})
+app.get("/api/persons", (request, response) => {
+  response.status(200).json(persons);
+});
 
-app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info on ${persons.length} people</p>
-    <p>${new Date()}</p>`);    
-})
+app.get("/info", (request, response) => {
+  response.send(`<p>Phonebook has info on ${persons.length} people</p>
+    <p>${new Date()}</p>`);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = +request.params.id;
-    const entry = persons.find(p => p.id === id);
-    // console.log(entry);
-    if (entry){
-        response.status(200).send(entry);
-    } else {
-        response.status(404).send("<h3>Entry does not exist</h3>");
-    }
-})
+app.get("/api/persons/:id", (request, response) => {
+  const id = +request.params.id;
+  const entry = persons.find((p) => p.id === id);
+  // console.log(entry);
+  if (entry) {
+    response.status(200).send(entry);
+  } else {
+    response.status(404).send("<h3>Entry does not exist</h3>");
+  }
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = +request.params.id;
-    persons = persons.filter(p => p.id !== id);
-    
-    response.status(204).end();
-})
+app.delete("/api/persons/:id", (request, response) => {
+  const id = +request.params.id;
+  persons = persons.filter((p) => p.id !== id);
 
-app.post('/api/persons/', (request, response) => {
+  response.status(204).end();
+});
+
+app.post("/api/persons/", (request, response, next) => {
   const body = request.body;
-  if (!body.name){
+  if (!body.name) {
     response.status(400).json({
-      error: "entry name is missing"
-    })
-  } else if(!body.number){
+      error: "entry name is missing",
+    });
+    return;
+  } else if (!body.number) {
     response.status(400).json({
-      error: "entry is missing number phone"
-    })
-  } else if(persons.find(p => p.name === body.name)){
+      error: "entry is missing number phone",
+    });
+    return;
+  } else if (persons.find((p) => p.name === body.name)) {
+    // console.log(response);
     response.status(406).json({
-      error: "Server does not allow duplicate entry"
-    })
+      error: "Server does not allow duplicate entry",
+    });
+    return;
   }
 
   const newEntry = {
     id: generateId(),
     name: body.name,
     number: body.number,
-  }
+  };
 
   persons = persons.concat(newEntry);
   response.json(newEntry);
-})
+  assignContent(request, response, next, newEntry);
+});
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+// const unknownEndpoint = (request, response, next) => {
+//   response.status(404).json({ error: "unknown endpoint" });
+//   next();
+// };
 
-app.use(unknownEndpoint);
+// app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Running Server on Port ${PORT}`);
-})
+  console.log(`Running Server on Port ${PORT}`);
+});
 
 let generateId = () => {
-  return Math.floor(Math.random() * (2**16));
+  return Math.floor(Math.random() * 2 ** 16);
+};
+
+function assignContent(req, res, next, entry) {
+  req.content = JSON.stringify(entry);
+  next();
 }
+
+
