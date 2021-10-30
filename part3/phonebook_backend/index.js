2f2c2bd6314b4ceb2a81dc224b5b8db6aa4731c2
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-10-30 21:30:18 +0300

    ex 3.7 add morgan middleware

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan")
 
 app.use(express.json());
+app.use(morgan('tiny'))
 
 let persons = [
   {
@@ -82,6 +84,12 @@ app.post('/api/persons/', (request, response) => {
   persons = persons.concat(newEntry);
   response.json(newEntry);
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
