be379aff5a43ad41216eda9bdcdca338c91254d7
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-10-31 10:47:38 +0200

    preparing backend for heroku deployment

--- part3/notes_node/index.js
+++ part3/notes_node/index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 let notes = [
   {
@@ -80,7 +82,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
