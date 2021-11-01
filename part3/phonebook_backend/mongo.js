3c053bf9953419f40b695145932e56824028979c
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-02 01:03:30 +0200

    Ex 3.12; add record; fetch records

--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -1,33 +1,48 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 5){
-    console.log('Bad arguments length: node mongo.js <password> <phonebook_user> <user_number>');
-    process.exit(1);
-}
-
 const password = process.argv[2];
 const entry_name = process.argv[3];
 const entry_number = process.argv[4];
 
 const url = `mongodb+srv://data_master:${password}@cluster0.zocpq.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
 
+if (process.argv.length < 3 && process.argv.length > 5){
+    console.log(`Bad arguments length: node mongo.js <password> <phonebook_user> <user_number> to add a user
+    node mongo.js <password> to query for all users`);
+    process.exit(1);
+}
+
 mongoose.connect(url);
 
 const phonebook_schema = mongoose.Schema({
-    name: String,
-    number: String,
-    show: Boolean
-})
+  name: String,
+  number: String,
+  show: Boolean,
+});
 
-const Entry = mongoose.model('Entry', phonebook_schema)
+const Entry = mongoose.model("Entry", phonebook_schema);
 
-const entry = new Entry({
-    name: "Brian Mckenah",
-    number: '02-326548',
-    show: true,
-})
+if(process.argv.length === 5){
 
-entry.save().then(result => {
-    console.log(`Added ${entry_name} number ${entry_number} to phonebook`);
-    mongoose.connection.close();
-})
+    const entry = new Entry({
+      name: entry_name,
+      number: entry_number,
+      show: true,
+    });
+
+    entry.save().then((result) => {
+      console.log(`Added ${entry_name} number ${entry_number} to phonebook`);
+      mongoose.connection.close();
+    });   
+} else if (process.argv.length === 3){
+
+    console.log('phonebook:');    
+    Entry.find({}).then(result => {
+        result.forEach(e => {
+            console.log(`${e.name} ${e.number}`);
+        })
+        mongoose.connection.close()
+    })
+}
+
+
