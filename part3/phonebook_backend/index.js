9199f763e702f483fd28678bf75385d9e1e15ed9
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-02 20:04:20 +0200

    Ex 3.13 fetch data from database, frontend works

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require('cors');
+const Entry = require('./model/entry')
 
 app.use(express.json());
 app.use(express.static('build'))
@@ -75,7 +76,9 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.status(200).json(persons);
+  Entry.find({}).then(result => {
+    response.status(200).json(result);
+  })
 });
 
 app.get("/info", (request, response) => {
---
+++ part3/phonebook_backend/model/entry.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const mongoose = require("mongoose")
+
+const url = process.env.MONGODB_URI;
+
+mongoose.connect(url).then(result => {
+    console.log("Connected to phonebook-app mongodb")
+}).catch(error => {
+    console.log("Error connecting to database", error.message);
+})
+
+const EntrySchema = mongoose.Schema({
+    name: String,
+    number: String,
+    show: Boolean
+})
+
+EntrySchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+// const Entry = mongoose.model('Entry', EntrySchema);
+
+module.exports = mongoose.model('Entry', EntrySchema);
