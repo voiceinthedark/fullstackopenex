e61eb94cae984d4a343612221fe015814a064955
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-03 22:56:17 +0200

    notes-backend add validation

--- part3/notes_node/index.js
+++ part3/notes_node/index.js
@@ -15,6 +15,8 @@ const errorHandler = (error, request, response, next) => {
 
   if(error.name === 'CastError'){
     return response.status(400).send({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message })
   }
 
   next(error);
@@ -75,7 +77,7 @@ app.delete('/api/notes/:id', (request, response) => {
   })
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
   
   if(!body.content){
@@ -93,6 +95,7 @@ app.post('/api/notes', (request, response) => {
   note.save().then(result => {
     response.json(result);
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
